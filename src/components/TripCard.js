b71430a3c420fe59741d3264e64cf98f29c18d98
alelizar <aleksandra.elizarova@t-systems.com>
2021-03-16 13:22:33 +0300

    make rating a separated component

--- src/components/TripCard.js
+++ src/components/TripCard.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Rating } from "../components/Rating";
+
 
 export function TripCard() {
     return (
@@ -7,7 +9,8 @@ export function TripCard() {
             <div className="p-5 pb-0">
                 <h1 className="placeName text-2xl font-bold text-green-800 py-2">Iceland</h1>
                 <p className="dates bg-white text-base text-black">01.05.2021-14.05.2021</p>
-                <div class="flex space-x-3">
+                <Rating></Rating>
+                {/* <div class="flex space-x-3">
                     <svg class="w-8 h-8 text-yellow-300" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
                         <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
                     </svg>
@@ -23,7 +26,7 @@ export function TripCard() {
                     <svg class="w-8 h-8 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
                         <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
                     </svg>
-                </div>
+                </div> */}
                 <a href="" className="py-2 px-3 mt-4 px-8 text-white bg-blue-400 inline-block rounded">Read More</a>
             </div>
         </div>
--- src/components/TripCardLong.js
+++ src/components/TripCardLong.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Rating } from "../components/Rating";
 
 export function TripCardLong() {
     return (
@@ -11,7 +12,8 @@ export function TripCardLong() {
                 <p id="job" className="text-gray-800 mt-2">Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reiciendis vitae qui distinctio ex soluta? Voluptates, ea! Esse, natus. Quas possimus laboriosam consectetur deserunt ea sapiente. Dicta ipsam atque voluptatem!</p>
                 <div className="flex mt-5">
                     {/* <img alt="avatar" className="w-6 rounded-full border-2 border-gray-300" src="https://picsum.photos/seed/picsum/200" /> */}
-                    <div class="flex space-x-3">
+                    <Rating></Rating>
+                    {/* <div class="flex space-x-3">
                         <svg class="w-8 h-8 text-yellow-300" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
                             <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
                         </svg>
@@ -27,7 +29,7 @@ export function TripCardLong() {
                         <svg class="w-8 h-8 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
                             <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
                         </svg>
-                    </div>
+                    </div> */}
                 </div>
             </div>
         </div>
