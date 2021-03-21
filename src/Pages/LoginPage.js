9d46705a97586845bd8fa6a18824a18edd0079a8
alelizar <aleksandra.elizarova@t-systems.com>
2021-03-21 22:10:12 +0300

    changed bg

--- src/Pages/LoginPage.js
+++ src/Pages/LoginPage.js
@@ -5,15 +5,15 @@ export function LoginPage() {
     <div class="w-full flex flex-wrap">
       {/* <!-- Image Section --> */}
       <div class="w-1/2 shadow-2xl">
-        <img class="object-cover w-full h-screen hidden md:block" src="https://source.unsplash.com/IXUM4cJynP0"></img>
+        <img class="object-cover w-full h-screen hidden md:block" src="https://images.unsplash.com/photo-1539639087565-41c59f40f3d7?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80"></img>
       </div>
 
       {/* <!-- Login Section --> */}
       <div class="w-full md:w-1/2 flex flex-col">
 
-        <div class="flex justify-center md:justify-start pt-12 md:pl-12 md:-mb-24">
+        {/* <div class="flex justify-center md:justify-start pt-12 md:pl-12 md:-mb-24">
           <a href="#" class="bg-black text-white font-bold text-xl p-4">Logo</a>
-        </div>
+        </div> */}
 
         <div class="flex flex-col justify-center md:justify-start my-auto pt-8 md:pt-0 px-8 md:px-24 lg:px-32">
           <p class="text-center text-3xl">Welcome.</p>
