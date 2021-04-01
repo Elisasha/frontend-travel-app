926ccd355c3fbcdb5b2c6e1f48ccddb597694b79
alelizar <aleksandra.elizarova@t-systems.com>
2021-04-01 15:08:21 +0300

    add/remove city btn

--- src/Pages/AddTrip.jsx
+++ src/Pages/AddTrip.jsx
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { Calendar } from "../components/Calendar";
 import { UserCard } from "../components/UserCard";
 import '../components/Calendar.css';
 
 export function AddTrip() {
+    const [fields, setFields] = useState([{ value: null }]);
+
+    function handleChange(i, event) {
+        const values = [...fields];
+        values[i].value = event.target.value;
+        setFields(values);
+    }
+
+    function handleAdd() {
+        const values = [...fields];
+        values.push({ value: null });
+        setFields(values);
+    }
+
+    function handleRemove(i) {
+        const values = [...fields];
+        values.splice(i, 1);
+        setFields(values);
+    }
+
     return (
         <section className="section">
             <div className="container mx-auto">
@@ -17,12 +37,21 @@ export function AddTrip() {
                             </div>
                             <Calendar />
                             <label for="city" class="text-lg  pt-4">Cities</label>
-                            <div class="flex flex-wrap items-stretch w-full mb-4 relative xl:w-2/5">
-                                <input type="city" placeholder="Reykjavik" class="appearance-none flex-shrink flex-grow flex-auto leading-normal focus:outline-none w-px border h-10 px-3 relative" ></input>
-                                <div class="flex -mr-px">
-                                    <button class="appearance-none flex items-center leading-normal bg-grey-lighter rounded-l-none border border-l-0 px-3 whitespace-no-wrap text-grey-dark text-sm">+</button>
-                                </div>
-                            </div>
+                            {fields.map((field, idx) => {
+                                return (
+                                    <div key={`${field}-${idx}`}>
+                                        <div class="flex flex-wrap items-stretch w-full mb-4 relative xl:w-2/5">
+                                            <input type="city" value={field.value || ""} onChange={e => handleChange(idx, e)} placeholder="Reykjavik" class="appearance-none flex-shrink flex-grow flex-auto leading-normal focus:outline-none border h-10 px-3 relative" ></input>
+                                            <div class="flex -mr-px">
+                                                {idx > 0
+                                                    ? <button type="button" onClick={() => handleRemove()} class="w-8 appearance-none flex items-center leading-normal bg-grey-lighter rounded-l-none border border-l-0 px-3 whitespace-no-wrap text-grey-dark text-sm">-</button>
+                                                    : <button type="button" onClick={() => handleAdd()} class="w-8 w-0appearance-none flex items-center leading-normal bg-grey-lighter rounded-l-none border border-l-0 px-3 whitespace-no-wrap text-grey-dark text-sm">+</button>
+                                                }
+                                            </div>
+                                        </div>
+                                    </div>
+                                );
+                            })}
                             <div class="flex flex-col pt-4 xl:w-2/5">
                                 <input type="submit" value="Add" class="bg-black text-white font-bold text-lg p-2 mt-8"></input>
                             </div>
