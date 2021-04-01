9d971080c8bc709146f99fe9a49df4f70c54afae
alelizar <aleksandra.elizarova@t-systems.com>
2021-04-02 00:36:37 +0300

    friends list correction

--- src/Pages/FriendsList.js
+++ src/Pages/FriendsList.js
@@ -10,7 +10,8 @@ export function FriendsList() {
                 <main className="main">
                     <UserCard />
                     <div className="content-area bg-white rounded overflow-hidden shadow-lg">
-                        <div class="grid gap-1 grid-cols-1 sm:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8 mt-6 px-4">
+                        {/* <div class="grid gap-1 grid-cols-1 sm:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8 mt-6 px-4"> */}
+                        <div class="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-6 mt-6 px-4">
                             {Array.from({ length: 10 }).map(index => <FriendCard key={index}></FriendCard>)}
                         </div>
                     </div>
--- src/components/FriendCard.js
+++ src/components/FriendCard.js
@@ -4,11 +4,11 @@ export function FriendCard() {
     return (
         <div className="text-center p-6">
             <img
-                className="h-24 w-24 rounded-full mx-auto"
+                className="h-24 w-24 rounded-md mx-auto"
                 src="https://randomuser.me/api/portraits/women/22.jpg"
-                alt="Edith Piaf"
+                alt="Friend Name"
             />
-            <p className="pt-2 text-lg font-semibold">Edith Piaf</p>
+            <p className="pt-2 text-lg font-semibold">Friend Name</p>
         </div>
     );
 }
