cd8d4e0c5dd62314da35354ea347cae952a7e3b9
alelizar <aleksandra.elizarova@t-systems.com>
2021-04-02 12:19:29 +0300

    containerization

--- src/Pages/AddTrip.jsx
+++ src/Pages/AddTrip.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Calendar } from "../components/Calendar";
-import { UserCard } from "../components/UserCard";
 import '../components/Calendar.css';
+import { Container } from "../components/Container";
 
 export function AddTrip() {
     const [cities, setCities] = useState([{ value: null }]);
@@ -25,40 +25,35 @@ export function AddTrip() {
     }
 
     return (
-        <section className="section">
-            <div className="container mx-auto">
-                <main className="main">
-                    <UserCard />
-                    <div className="content-area bg-white rounded overflow-hidden shadow-lg p-10">
-                        <form class="flex flex-col pt-3" onsubmit="event.preventDefault();">
-                            <div class="flex flex-col pt-4 xl:w-2/5">
-                                <label for="tripName" class="text-lg">Trip name</label>
-                                <input id="tripName" placeholder="Iceland" class="appearance-none border w-full py-2 px-3 text-gray-700 mt-1 leading-tight focus:outline-none focus:shadow-outline"></input>
-                            </div>
-                            <Calendar />
-                            <label for="city" class="text-lg  pt-4">Cities</label>
-                            {cities.map((city, idx) => {
-                                return (
-                                    <div key={`${city}-${idx}`}>
-                                        <div class="flex flex-wrap items-stretch w-full mb-4 relative xl:w-2/5">
-                                            <input type="city" value={city.value || ""} onChange={e => handleChange(idx, e)} placeholder="Reykjavik" class="appearance-none flex-shrink flex-grow flex-auto leading-normal focus:outline-none border h-10 px-3 relative" ></input>
-                                            <div class="flex -mr-px">
-                                                {idx > 0
-                                                    ? <button type="button" onClick={() => handleRemove(idx)} class="w-8 appearance-none flex items-center leading-normal bg-grey-lighter rounded-l-none border border-l-0 px-3 whitespace-no-wrap text-grey-dark text-sm">-</button>
-                                                    : <button type="button" onClick={() => handleAdd()} class="w-8 w-0appearance-none flex items-center leading-normal bg-grey-lighter rounded-l-none border border-l-0 px-3 whitespace-no-wrap text-grey-dark text-sm">+</button>
-                                                }
-                                            </div>
-                                        </div>
-                                    </div>
-                                );
-                            })}
-                            <div class="flex flex-col pt-4 xl:w-2/5">
-                                <input type="submit" value="Add" class="bg-black text-white font-bold text-lg p-2 mt-8"></input>
-                            </div>
-                        </form>
+        <Container>
+            <div className="p-10">
+                <form class="flex flex-col pt-3" onsubmit="event.preventDefault();">
+                    <div class="flex flex-col pt-4 xl:w-2/5">
+                        <label for="tripName" class="text-lg">Trip name</label>
+                        <input id="tripName" placeholder="Iceland" class="appearance-none border w-full py-2 px-3 text-gray-700 mt-1 leading-tight focus:outline-none focus:shadow-outline"></input>
                     </div>
-                </main>
+                    <Calendar />
+                    <label for="city" class="text-lg  pt-4">Cities</label>
+                    {cities.map((city, idx) => {
+                        return (
+                            <div key={`${city}-${idx}`}>
+                                <div class="flex flex-wrap items-stretch w-full mb-4 relative xl:w-2/5">
+                                    <input type="city" value={city.value || ""} onChange={e => handleChange(idx, e)} placeholder="Reykjavik" class="appearance-none flex-shrink flex-grow flex-auto leading-normal focus:outline-none border h-10 px-3 relative" ></input>
+                                    <div class="flex -mr-px">
+                                        {idx > 0
+                                            ? <button type="button" onClick={() => handleRemove(idx)} class="w-8 appearance-none flex items-center leading-normal bg-grey-lighter rounded-l-none border border-l-0 px-3 whitespace-no-wrap text-grey-dark text-sm">-</button>
+                                            : <button type="button" onClick={() => handleAdd()} class="w-8 w-0appearance-none flex items-center leading-normal bg-grey-lighter rounded-l-none border border-l-0 px-3 whitespace-no-wrap text-grey-dark text-sm">+</button>
+                                        }
+                                    </div>
+                                </div>
+                            </div>
+                        );
+                    })}
+                    <div class="flex flex-col pt-4 xl:w-2/5">
+                        <input type="submit" value="Add" class="bg-black text-white font-bold text-lg p-2 mt-8"></input>
+                    </div>
+                </form>
             </div>
-        </section>
+        </Container>
     );
 }
--- src/Pages/TripList.js
+++ src/Pages/TripList.js
@@ -1,23 +1,16 @@
 import React from 'react';
-import { UserCard } from "../components/UserCard";
 import { TripCard } from "../components/TripCard";
+import { Container } from '../components/Container';
 
 
 export function Triplist() {
     return (
-        <section className="section">
-            <div className="md:container md:mx-auto">
-                <main className="main">
-                    <UserCard />
-                    <div className="content-area bg-white rounded overflow-hidden shadow-lg">
-                        {/* TODO remove style */}
-                        {/* <div style={{ height: 3000, background: 'salmon' }}></div> */}
-                        <div class="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-6 px-4">
-                            {Array.from({ length: 10 }).map(index => <TripCard key={index}></TripCard>)}
-                        </div>
-                    </div>
-                </main>
+        <Container>
+            {/* TODO remove style */}
+            {/* <div style={{ height: 3000, background: 'salmon' }}></div> */}
+            <div class="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-6 px-4">
+                {Array.from({ length: 10 }).map(index => <TripCard key={index}></TripCard>)}
             </div>
-        </section>
+        </Container>
     );
 }
