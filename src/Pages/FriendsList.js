657a30fa38937ec17e7d357e1c00104dc6bdc3ac
alelizar <aleksandra.elizarova@t-systems.com>
2021-04-07 18:59:03 +0300

    class -> className

--- src/Pages/FriendsList.js
+++ src/Pages/FriendsList.js
@@ -6,8 +6,8 @@ import { Container } from '../components/Container';
 export function FriendsList() {
     return (
         <Container>
-            {/* <div class="grid gap-1 grid-cols-1 sm:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8 mt-6 px-4"> */}
-            <div class="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-6 mt-6 px-4">
+            {/* <div className="grid gap-1 grid-cols-1 sm:grid-cols-4 lg:grid-cols-6 xl:grid-cols-8 mt-6 px-4"> */}
+            <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-6 mt-6 px-4">
                 {Array.from({ length: 10 }).map(index => <FriendCard key={index}></FriendCard>)}
             </div>
         </Container>
--- src/Pages/TripList.js
+++ src/Pages/TripList.js
@@ -9,7 +9,7 @@ export function Triplist() {
         <Container>
             <SearchPanel></SearchPanel>
             {/* <div style={{ height: 3000, background: 'salmon' }}></div> */}
-            <div class="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-6 px-4">
+            <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-6 px-4">
                 {Array.from({ length: 10 }).map(index => <TripCard key={index}></TripCard>)}
             </div>
         </Container >
