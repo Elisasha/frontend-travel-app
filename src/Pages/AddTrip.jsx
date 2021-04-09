72c0d7746671c1219e724d758bfb6ada4c8fc81f
alelizar <aleksandra.elizarova@t-systems.com>
2021-04-09 21:30:34 +0300

    add trip form submit

--- src/Pages/AddTrip.jsx
+++ src/Pages/AddTrip.jsx
@@ -2,9 +2,14 @@ import React, { useState } from "react";
 import { Calendar } from "../components/Calendar";
 import '../components/Calendar.css';
 import { Container } from "../components/Container";
+import firebase from '../base';
+import app from "../base.js";
 
 export function AddTrip() {
     const [cities, setCities] = useState([{ value: null }]);
+    const [country, setCountry] = useState();
+    const [tripID, setTripID] = useState();
+    //startDate endDate ??
 
     function handleChange(i, event) {
         const values = [...cities];
@@ -24,13 +29,25 @@ export function AddTrip() {
         setCities(values);
     }
 
+    function handleSubmit() {
+        setTripID(Math.random().toString(36).substr(2, 9))
+        // database.ref() and firebase.database().ref ask Slava
+        firebase.database().ref('trips/' + tripID).set({
+            country: country,
+            // startDate: document.getElementById('tata-start-date').value,
+            // endDate: document.getElementById('tata-end-date').value,
+            cities: cities
+        });
+        console.log('send to db: country:' + country + ', cities:' + cities)
+    }
+
     return (
         <Container>
             <div className="p-10">
-                <form className="flex flex-col pt-3" onsubmit="event.preventDefault();">
+                <form className="flex flex-col pt-3" onSubmit= {(event) => event.preventDefault()}>
                     <div className="flex flex-col pt-4 xl:w-2/5">
                         <label for="tripName" className="text-lg">Trip name</label>
-                        <input id="tripName" placeholder="Iceland" className="appearance-none border w-full py-2 px-3 text-gray-700 mt-1 leading-tight focus:outline-none focus:shadow-outline"></input>
+                        <input id="tripName" placeholder="Iceland" value={country} onChange={e => setCountry(e.target.value)} className="appearance-none border w-full py-2 px-3 text-gray-700 mt-1 leading-tight focus:outline-none focus:shadow-outline"></input>
                     </div>
                     <Calendar />
                     <label for="city" className="text-lg  pt-4">Cities</label>
@@ -49,9 +66,9 @@ export function AddTrip() {
                             </div>
                         );
                     })}
-                    <div className="flex flex-col pt-4 xl:w-2/5">
-                        <input type="submit" value="Add" className="bg-black text-white font-bold text-lg p-2 mt-8"></input>
-                    </div>
+                        <div className="flex flex-col pt-4 xl:w-2/5">
+                            <input type="submit" onClick={() => handleSubmit()} value="Add" className="bg-black text-white font-bold text-lg p-2 mt-8"></input>
+                        </div>
                 </form>
             </div>
         </Container>
