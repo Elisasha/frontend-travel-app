83e4f379fd27d87c140e5b4e694f0b8f92f98db5
alelizar <aleksandra.elizarova@t-systems.com>
2021-04-09 22:08:32 +0300

    date fields and id submit

--- src/Pages/AddTrip.jsx
+++ src/Pages/AddTrip.jsx
@@ -3,13 +3,11 @@ import { Calendar } from "../components/Calendar";
 import '../components/Calendar.css';
 import { Container } from "../components/Container";
 import firebase from '../base';
-import app from "../base.js";
 
 export function AddTrip() {
     const [cities, setCities] = useState([{ value: null }]);
     const [country, setCountry] = useState();
     const [tripID, setTripID] = useState();
-    //startDate endDate ??
 
     function handleChange(i, event) {
         const values = [...cities];
@@ -34,17 +32,18 @@ export function AddTrip() {
         // database.ref() and firebase.database().ref ask Slava
         firebase.database().ref('trips/' + tripID).set({
             country: country,
-            // startDate: document.getElementById('tata-start-date').value,
-            // endDate: document.getElementById('tata-end-date').value,
+            startDate: document.getElementById('tata-start-date').value,
+            endDate: document.getElementById('tata-end-date').value,
             cities: cities
         });
-        console.log('send to db: country:' + country + ', cities:' + cities)
+        //add redirect to trip page
+        console.log('send to db: trip id[' + tripID + ']')
     }
 
     return (
         <Container>
             <div className="p-10">
-                <form className="flex flex-col pt-3" onSubmit= {(event) => event.preventDefault()}>
+                <form className="flex flex-col pt-3" onSubmit={(event) => event.preventDefault()}>
                     <div className="flex flex-col pt-4 xl:w-2/5">
                         <label for="tripName" className="text-lg">Trip name</label>
                         <input id="tripName" placeholder="Iceland" value={country} onChange={e => setCountry(e.target.value)} className="appearance-none border w-full py-2 px-3 text-gray-700 mt-1 leading-tight focus:outline-none focus:shadow-outline"></input>
@@ -66,9 +65,9 @@ export function AddTrip() {
                             </div>
                         );
                     })}
-                        <div className="flex flex-col pt-4 xl:w-2/5">
-                            <input type="submit" onClick={() => handleSubmit()} value="Add" className="bg-black text-white font-bold text-lg p-2 mt-8"></input>
-                        </div>
+                    <div className="flex flex-col pt-4 xl:w-2/5">
+                        <input type="submit" onClick={() => handleSubmit()} value="Add" className="bg-black text-white font-bold text-lg p-2 mt-8"></input>
+                    </div>
                 </form>
             </div>
         </Container>
