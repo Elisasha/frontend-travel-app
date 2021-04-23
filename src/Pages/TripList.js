3995c5cd3389a51053c5b22b000d036ef27590d3
alelizar <aleksandra.elizarova@t-systems.com>
2021-04-23 22:07:10 +0300

    triplist retrieve

--- src/Pages/TripList.js
+++ src/Pages/TripList.js
@@ -6,13 +6,14 @@ import { useDispatch, useSelector } from 'react-redux';
 
 
 export function Triplist() {
-    const { curUser, users, trips } = useSelector((state) => { return { curUser: state.curUser, users: state.users, trips: state.trips } });
+    const { curUser, users, trips } = useSelector((state) => { return { curUser: state.curUser, users: state.users, trips: Object.values(state.trips) } });
     const dispatch = useDispatch();
     React.useEffect(() => { dispatch(getUserTrips(curUser)) }, [])
     return (
         <Container>
             <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-1 mb-4 px-4">
-                {Object.values(trips).map((trip, index) => <TripCard {...trip} key={index}></TripCard>)}
+                {/* {Object.values(trips).map((trip, index) => <TripCard {...trip} key={index}></TripCard>)} */}
+                {trips.map((trip, index) => <TripCard {...trip} key={index}></TripCard>)}
             </div>
         </Container >
     );
