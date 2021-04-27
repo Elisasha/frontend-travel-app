45cae97ba053f1553e4dfe762cb28c3daece2354
alelizar <aleksandra.elizarova@t-systems.com>
2021-04-27 19:16:38 +0300

    sort done

--- src/Pages/FriendsList.js
+++ src/Pages/FriendsList.js
@@ -5,39 +5,127 @@ import { useDispatch, useSelector } from "react-redux";
 import { getUserFriendRequests, getUserFriends } from "../store/users/actions";
 
 export function FriendsList() {
-  const { curUser, users } = useSelector((state) => {
-    return { curUser: state.curUser, users: state.users };
-  });
   const dispatch = useDispatch();
   React.useEffect(() => {
     dispatch(getUserFriends(curUser));
     dispatch(getUserFriendRequests(curUser));
   }, []);
+  const { curUser, users, friendRequests, friends, filter } = useSelector(
+    (state) => {
+      const frReqArr = [];
+      const frArr = [];
+
+      if (
+        state.curUser.friendRequests &&
+        state.curUser.friendRequests.length > 0
+      ) {
+        state.curUser.friendRequests.forEach((frR) => {
+          frReqArr.push(state.users[frR]);
+        });
+      }
+
+      if (state.curUser.friends && state.curUser.friends.length > 0) {
+        state.curUser.friends.forEach((fr) => {
+          frArr.push(state.users[fr]);
+        });
+      }
+
+      // function sortArr(array) {
+      if (frReqArr.length > 0) {
+        if (state.sort.order === "ASC") {
+          frReqArr.sort((a, b) =>
+            a["displayName"] < b["displayName"]
+              ? -1
+              : a["displayName"] > b["displayName"]
+              ? 1
+              : 0
+          );
+        } else {
+          frReqArr.sort((a, b) =>
+            a["displayName"] > b["displayName"]
+              ? -1
+              : a["displayName"] < b["displayName"]
+              ? 1
+              : 0
+          );
+        }
+      }
+
+      if (frArr.length > 0) {
+        if (state.sort.order === "ASC") {
+          frArr.sort((a, b) =>
+            a["displayName"] < b["displayName"]
+              ? -1
+              : a["displayName"] > b["displayName"]
+              ? 1
+              : 0
+          );
+        } else {
+          frArr.sort((a, b) =>
+            a["displayName"] > b["displayName"]
+              ? -1
+              : a["displayName"] < b["displayName"]
+              ? 1
+              : 0
+          );
+        }
+      }
+
+      return {
+        curUser: state.curUser,
+        users: state.users,
+        friendRequests: frReqArr,
+        friends: frArr,
+        filter: state.sort.filter.toLowerCase,
+      };
+    }
+  );
+
+  // friends.forEach((fr) => console.log(fr."displayName".includes(filter)));
+  console.log(friends);
+  console.log(friendRequests);
+
   return (
     <Container>
-      {curUser.friendRequests.length > 0 && (
+      {friendRequests.length > 0 && (
         <h3 className="pt-2 text-center text-lg font-semibold">
-          Friend request {curUser.friendRequests.length > 1 ? `s` : ``}
+          Friend request{friendRequests.length > 1 ? `s` : ``}
         </h3>
       )}
       <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-6 mt-6 px-4">
-        {curUser.friendRequests
-          .map((frID) => users[frID])
-          .map((friend, index) => (
-            <FriendCard {...friend} key={index}></FriendCard>
-          ))}
+        {friendRequests.length > 0
+          ? friendRequests
+              // .filter((frReq) => {
+              //   if (frReq."displayName".toLowerCase().includes(filter)) {
+              //     return true;
+              //   }
+              //   return false;
+              // })
+              // .map((frID) => users[frID])
+              .map((friend, index) => (
+                <FriendCard {...friend} key={index}></FriendCard>
+              ))
+          : ""}
       </div>
-      {curUser.friends.length > 0 && (
+      {friends.length > 0 && (
         <h3 className="pt-2 text-center text-lg font-semibold">
-          My friend {curUser.friends.length > 1 ? `s` : ``}
+          My friend{friends.length > 1 ? `s` : ``}
         </h3>
       )}
       <div className="grid gap-4 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-6 mt-6 px-4">
-        {curUser.friends
-          .map((frID) => users[frID])
-          .map((friend, index) => (
-            <FriendCard {...friend} key={index}></FriendCard>
-          ))}
+        {friends.length > 0
+          ? friends
+              // .map((frID) => users[frID])
+              // .filter((fr) => {
+              //   if (fr."displayName".toLowerCase().includes(filter)) {
+              //     return true;
+              //   }
+              //   return false;
+              // })
+              .map((friend, index) => (
+                <FriendCard {...friend} key={index}></FriendCard>
+              ))
+          : ""}
       </div>
     </Container>
   );
